e35f6050b049a1dc19bf77792037e827540b78f8
Robert Parker <roberthparker86@gmail.com>
2021-02-06 20:09:14 -0600

    Refactor gameplay.js. Breakdown scoreboard button control into series of smaller functions to reduce repetative code and increase readability

--- js/gameplay.js
+++ js/gameplay.js
@@ -2,50 +2,56 @@
 $(function() { // .ready method shorthand
 
  /////Ctrls for the score buttons /////
+  const checkHold = (elem) => $(elem).hasClass("hold");
+
+  const checkRoundZero = () => round === 0;
+
+  const message = (string) => { // Pops up message window with passed string
+    $(".message").text(string);
+    setTimeout( () => {
+      $(".message").show();
+    }, 20);
+  };
+
+  const setScore = (elem) => {
+    const returnScoreClass = (elem) => { // return the appropriate selector class
+      return ($(elem).hasClass("score-btn"))
+        ? ".score-val"
+        : ($(elem).hasClass("score-btn-b"))
+          ? ".score-val-b"
+          : alert("returnScoreClass Error");
+    };
+
+    const scoreRow = $(elem).parents(".score-row"),
+      scoreVal = scoreRow.find(returnScoreClass(elem)),
+      value = calcScore($(elem).attr("id")),
+      total = plyrScore.reduce( (a,b) => a + b );
+
+    scoreVal.text(value);
+    scoreVal.addClass("hold");
+    $("#total").text(total);
+    $(elem).addClass("hold");
+    plyrTurn++;
+    setTimeout(plyrCheck, 500);
+  };
+
+  const handleScoreClick = (elem) => { // If problem display message otherwise set score on scoreboard
+    checkHold(elem) 
+    ? message("This category has been used. Pick another.")
+    : checkRoundZero()
+      ? message("Roll dice to begin game!")
+      : setScore(elem);
+  };
 
   $(".score-btn").on("click", function() { //  Ctrls the left column
-    if ($(this).hasClass("hold" ) == true) {
-      $(".message").text("This category has been used. Pick another.");
-      setTimeout(function(){ $(".message").show(); }, 20);
-    } else if (round == 0) {
-      $(".message").text("Roll dice to begin game!");
-      setTimeout(function(){ $(".message").show(); }, 20);
-    } else {
-      let scoreRow = $(this).parents(".score-row"),
-          scoreVal = scoreRow.find(".score-val"),
-          value = calcScore($(this).attr("id")),
-          total = plyrScore.reduce(function(a,b){ return a + b;});
-      scoreVal.text(value);
-      scoreVal.addClass("hold");
-      $("#total").text(total);
-      $(this).addClass("hold");
-      plyrTurn++;
-      setTimeout(plyrCheck, 500);
-    }
+    handleScoreClick(this);
   });
 
   $(".score-btn-b").on("click", function() { // Ctrls right column
-    if ($(this).hasClass("hold") == true) {
-      $(".message").text("This category has been used. Pick another.");
-      setTimeout(function(){ $(".message").show(); }, 20);
-    } else if (round == 0) {
-      $(".message").text("Roll dice to begin game!");
-      setTimeout(function(){ $(".message").show(); }, 20);
-    } else {
-      let scoreRow = $(this).parents(".score-row"),
-          scoreVal = scoreRow.find(".score-val-b"),
-          value = calcScore($(this).attr("id")),
-          total = plyrScore.reduce(function(a,b){ return a + b;});
-      scoreVal.text(value);
-      scoreVal.addClass("hold");
-      $("#total").text(total);
-      $(this).addClass("hold");
-      plyrTurn++;
-      setTimeout(plyrCheck, 500);
-    }
+    handleScoreClick(this);
   });
 
-  $("body").on("click", function() {
+  $("body").on("click", function() { // Hide pop up message when screen is clicked
     if ($(".message").is(":visible")) {
       $(".message").hide();
     }
